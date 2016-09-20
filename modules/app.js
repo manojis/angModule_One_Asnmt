ef9b656b1c1a91b8248e1d243911ff04199f2d66
manoj_m <manojis@live.com>
2016-09-20 00:03:21 -0500

    First module assignment submission

---
+++ modules/app.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+angular.module('Lunch-Checker',[])
+.controller('lunchcheckerController', lunchcheckerController);
+
+lunchcheckerController.$inject = ['$scope'];
+function lunchcheckerController($scope){
+
+    $scope.lunchMenu="list comma separated dishes you usually have for lunch";
+    $scope.mess = "";
+    $scope.count = 0;
+
+      $scope.formControl = function(){
+        var noOfWords = $scope.lunchMenu.split(',');
+        var n=0;
+        for (var i=0 ; i < noOfWords.length ; i++){
+           if (noOfWords[i] != ""){
+             var n = n+1;
+           }
+        }
+        $scope.count = n;
+        return $scope.count;
+    };
+      $scope.message = function(){
+        if($scope.count>3){
+          $scope.mess = "Too much!"
+        }else{
+          $scope.mess = "Enjoy!"
+        }
+        return $scope.mess;
+      };
+}
+})();
